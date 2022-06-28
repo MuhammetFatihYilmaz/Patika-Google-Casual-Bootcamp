87447fdbaf4931eaf8a94a3bf9a2c53d9f06f8ab
MuhammetFatihYilmaz <muhammet.yilmaz.1995@gmail.com>
2022-06-28 22:32:11 +0300

    Added comment lines for planets rotate around sun and own axis.

--- Assets/Scripts/Planet Scripts/RotatePlanet.cs
+++ Assets/Scripts/Planet Scripts/RotatePlanet.cs
@@ -13,7 +13,9 @@ public class RotatePlanet : MonoBehaviour
 
     void Start()
     {
+        //Keeping start position.
         _startPos = transform.position;
+        //Start planet rotation sequence.
         StartCoroutine(RotatePlanetWithCoroutine());
     }
 
@@ -21,18 +23,22 @@ public class RotatePlanet : MonoBehaviour
     {
         while(true)
         {
+            //Wait for end of frame.
             yield return new WaitForEndOfFrame();
+            //Rotate planet around sun.
             transform.RotateAround(_sunTransform.position, Vector3.up, _rotateSpeedAroundSun * Time.deltaTime);
+            //Rotate planet around own axis.
             transform.Rotate(Vector3.up * _rotateSpeedOwnAxis * Time.deltaTime);
-            //
+            //Calculate  distance between starting position and current position.
             _distance = Vector3.Distance(transform.position, _startPos);
 
+            //If distance greater than one and if not logged that rotate complete, then continue.
             if (_distance > 1 && !_isLogged)
                 continue;
-
+            //Else if distance greater than one and if logged that rotate complete, then _isLogged set false.
             else if (_distance > 1 && _isLogged)
                 _isLogged = false;
-            //
+            //If distance greater than zero and distance less than point one, then log that rotate complete.
             if (_distance > 0 && _distance < 0.1f)
                 WriteLog();
         }
@@ -43,7 +49,9 @@ public class RotatePlanet : MonoBehaviour
     {
         if(!_isLogged)
         {
+            //Log that rotate complete.
             Debug.Log($"Complete {gameObject.name}",gameObject);
+            //_isLogged set true.
             _isLogged = true;
         }
     }
